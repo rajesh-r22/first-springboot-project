352fd8b72866986913fe73f0cecccda53745b1fd
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-12-22 02:07:39 +0530

    feat: Add CustomUserDetailsService for Spring Security and integrated with UserRepository

---
+++ src/main/java/com/example/learning/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.learning.service;
+
+import com.example.learning.entity.User;
+import com.example.learning.repository.UserRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
