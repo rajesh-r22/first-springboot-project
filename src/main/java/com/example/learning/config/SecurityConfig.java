adeb31146cf5f4e0015a7395382aa651d66ace97
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-12-23 03:14:10 +0530

    feat: Add AuthenticationManager bean int config file

--- src/main/java/com/example/learning/config/SecurityConfig.java
+++ src/main/java/com/example/learning/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.learning.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,9 +33,16 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
 
 }
