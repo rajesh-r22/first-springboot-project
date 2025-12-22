238a3cfe3421cdac9a2a7d53474da95b3094f756
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-12-23 03:33:43 +0530

    feat: Created UserLogin api in UserController

--- src/main/java/com/example/learning/controller/UserController.java
+++ src/main/java/com/example/learning/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.learning.controller;
 
+import com.example.learning.dto.LoginRequest;
 import com.example.learning.entity.User;
 import com.example.learning.service.UserService;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,20 @@ public class UserController {
     public String registration(@RequestBody User user){
         userService.register(user);
         return "User registered successfully";
+    }
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+
+         Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getUsername(),
+                                request.getPassword()
+                        )
+                );
+
+        return "Login Successful";
     }
 }
