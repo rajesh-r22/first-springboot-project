35fe0661dc07bc34b2e1a66c8a1070c556459b06
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2025-12-27 03:45:52 +0530

    feat(security):Add JwtService class in service layer for generate token logic

---
+++ src/main/java/com/example/learning/service/JwtService.java
@@ -0,0 +1,27 @@
+package com.example.learning.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import javax.crypto.spec.SecretKeySpec;
+
+
+
+@Component
+public class JwtService {
+
+    private final Key key= Keys.hmacShaKeyFor("verysecretkey@987654321".getBytes());
+    public String generateToken(String username){
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+1000*60*60))
+                .signWith(key)
+                .compact();
+    }
+
+}
