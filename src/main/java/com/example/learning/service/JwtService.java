70e191764b53efd6e7314154da6e9e626761fbc0
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-05 03:29:52 +0530

    Update expiration time from 60 mins to 10 min of access token

--- src/main/java/com/example/learning/service/JwtService.java
+++ src/main/java/com/example/learning/service/JwtService.java
@@ -18,7 +18,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*10))
                 .signWith(key)
                 .compact();
     }
