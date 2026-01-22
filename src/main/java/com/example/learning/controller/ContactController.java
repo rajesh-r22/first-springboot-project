a9b261da8040aabea72f62de6bb8e11ddeef47ff
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-23 04:23:33 +0530

    code clean up

--- src/main/java/com/example/learning/controller/ContactController.java
+++ src/main/java/com/example/learning/controller/ContactController.java
@@ -5,8 +5,6 @@ import com.example.learning.service.ContactService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
